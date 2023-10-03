0b22a1ec11f12b552cbaf72a9aa4bbbe7521da81
DungxLas <phamjhungfdungx@gmail.com>
2023-10-03 16:08:41 +0700

    03/10/2023

--- Native_Device_Features/components/Places/LocationPicker.tsx
+++ Native_Device_Features/components/Places/LocationPicker.tsx
@@ -1,10 +1,49 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from "react-native";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
 
-import OutlineButton from '../UI/OutlineButton';
-import { Colors } from '../../constants/color';
+import OutlineButton from "../UI/OutlineButton";
+import { Colors } from "../../constants/color";
 
 function LocationPicker() {
-  function getLocationHandler() {}
+  //const [pickedLocation, setPickedLocation] = useState("");
+
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+
+  async function verifyPermissions() {
+    if (
+      locationPermissionInformation!.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (locationPermissionInformation!.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant location permissions to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function getLocationHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const location = await getCurrentPositionAsync();
+    console.log(location);
+  }
 
   function pickOnMapHandler() {}
 
@@ -27,17 +66,17 @@ export default LocationPicker;
 
 const styles = StyleSheet.create({
   mapPreview: {
-    width: '100%',
+    width: "100%",
     height: 200,
     marginVertical: 8,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: Colors.primary100,
     borderRadius: 4,
   },
   actions: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
   },
 });
